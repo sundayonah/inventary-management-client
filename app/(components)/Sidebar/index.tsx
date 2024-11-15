f94bcf075706e7d7ea4d0c248e301b5bc165e1a4
sundayonah <sundayonah94@gmail.com>
2024-11-15 05:01:31 +0100

    initial commit

---
+++ app/(components)/Sidebar/index.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Menu } from "lucide-react"
+
+const Sidebar = () => {
+    return (
+        // TOP LOGO
+        <div>
+            <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+                <div>Logo</div>
+                <h1 className="font-extrabold text-2xl">ONAHSTOCK</h1>
+                <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => { }}>
+                    <Menu className="w-4 h-4" />
+                </button>
+            </div>
+            {/* LINKS */}
+            <div className="flex-grow mt-8">
+                {/* links here */}
+            </div>
+
+            {/* FOOTER */}
+            <div>
+                <p className="text-center text-xs text-gray-500">&copy; 2024 Onahstock</p>
+            </div>
+        </div>
+    )
+}
+export default Sidebar
